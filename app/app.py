f4539b33af7ace8ffd1bbcd8facd3fa31b82aa97
deethesaint <vtduong0912@gmail.com>
2025-05-08 15:40:17 +0700

    add scratches

--- app/app.py
+++ app/app.py
@@ -5,8 +5,8 @@ from PIL import Image
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
+@app.route('/ocr', methods=['GET', 'POST'])
+def ocr():
     extracted_text = ""
     error = ""
     if request.method == 'POST':
@@ -23,11 +23,16 @@ def home():
                 img = Image.open(io.BytesIO(img_bytes))
                 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
                 custom_config = r'-l vie+eng'
+                
                 extracted_text = pytesseract.image_to_string(img, config=custom_config)
             except Exception as e:
                 error = f"Failed to process image. Error: {str(e)}"
             return render_template('index.html', extracted_text=extracted_text, error=error)
     return render_template('index.html', extracted_text=extracted_text, error=error)
+
+@app.route('/speech-to-text', methods=['GET', 'POST'])
+def speech_to_text():
+    return render_template('speech-to-text.html', converted_text='')
 
 if __name__ == '__main__':
     app.run(debug=True) 
